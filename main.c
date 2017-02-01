4cef795e2187cc85de7f9e24b0d4f0ca723e567f
dr president <drpresident@localhost.localdomain>
2017-02-01 09:36:06 -0600

    reading member files works

--- main.c
+++ main.c
@@ -17,6 +17,10 @@ int main(int argc, char ** argv){
     messy_join_chan(network, "chan1");
     network->username = "hello";
     messy_join_chan(network, "chan1");
+    network->username = "newuser";
+    messy_join_chan(network, "chan1");
+
+    messy_cleanup(network);
 
     return 0;
 }
--- messy.h
+++ messy.h
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <errno.h>
 
@@ -34,27 +35,6 @@ char* _prefix = "/tmp/";
 int _prefixlen = 5;
 int _mem_size;
 
-void _free_messy_chan(messy_chan* c){
-    for(int i = 0; i < c->num_members; i++)
-        free(c->members[i]);
-    free(c->name);
-    free(c->path);
-    for(int i = 0; i < c->num_members; i++)
-        free(c->members[i]);
-    free(c->members);
-    free(c);
-}
-
-int _fsize(FILE *f){
-    fpos_t pos; 
-    fgetpos(f,&pos); 
-
-    fseek(f,0,SEEK_END);
-    int size = ftell(f);
-    fsetpos(f,&pos);
-
-    return size;
-}
 
 int messy_send_network(const char* network){
     return 1;
@@ -139,31 +119,35 @@ int messy_add_member(messy_chan *chan, const char* username){
     struct stat s;
     stat(chan->memfile, &s);
     int len = strlen(username),
-        size,
-        read;
+        read,
+        num_members;
     char *memlist = malloc(s.st_size + len + 3);
     char *tmp = malloc(sizeof(char) * 50);
 
     FILE *file = fopen(chan->memfile, "r+");
-    read = fscanf(file, "%i", &size);
-    size++;
+    read = fscanf(file, "%i", &num_members);
+    num_members++;
 
 
     //read current contents into memlist
-    sprintf(memlist, "%i", size);
+    sprintf(memlist, "%i", num_members);
     while(!feof(file)){
-        fscanf(file, "%s\n", tmp);
-        sprintf(memlist + strlen(memlist), "%s\n", tmp);
+        fscanf(file, "%s", tmp);
+        sprintf(memlist + strlen(memlist), "\n%s", tmp);
     }
     sprintf(memlist + strlen(memlist), "\n%s", username);
     printf("memlist:\n%s\n~\n", memlist);
 
-    //write edited contents
+    //close read file
     fclose(file);
+
+    //write edited contents
     file = fopen(chan->memfile, "w");
-    
     fwrite(memlist, strlen(memlist), 1, file);
     fclose(file);
+
+    free(memlist);
+    free(tmp);
 
     return 0;
 }
@@ -184,13 +168,22 @@ int messy_join_chan(messy_network *network, const char* chan_name){
         return 1;
 }
 
+void messy_set_username(messy_network *network, const char* username){
+    if(network->username)
+        free(network->username);
+
+    network->username = malloc(sizeof(char) * (strlen(username) + 1));
+    strcpy(network->username, username);
+}
+
 messy_network* messy_make_network(const char* network_name, 
                                   const char* username){
 
     int len = strlen(network_name);
     
     messy_network* network = malloc(sizeof(messy_network));
-    network->username = malloc(sizeof(char) * (strlen(username) + 1));
+    network->username = NULL;
+    messy_set_username(network, username);
     strcpy(network->username, username);
     removeWhitespace(&(network->username));
     network->chans = malloc(sizeof(messy_chan*) * MAX_CHANS); 
@@ -252,8 +245,23 @@ void NOTWORKING_make_hidden_dir(const char *path){
 }
 */
 
-void messy_cleanup(){
+void _free_messy_chan(messy_chan* c){
+    for(int i = 0; i < c->num_members; i++)
+        free(c->members[i]);
+    free(c->name);
+    free(c->path);
+    for(int i = 0; i < c->num_members; i++)
+        free(c->members[i]);
+    free(c->members);
+    free(c);
+}
 
+void messy_cleanup(messy_network *network){
+    free(network->username);
+    free(network->name);
+    free(network->path);
+    for(int i = 0; i < network->num_chans; i++)
+        _free_messy_chan(network->chans[i]);
 }
 
 void _grow(void* array, int size, int newsize){
